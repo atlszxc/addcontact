7fc4c75c677d52529bb75a7c39492747e42ba3b9
atlszxc <kiakovenko.reon@gmail.com>
2023-05-19 12:44:23 +0300

    update hook controller

--- index.js
+++ index.js
@@ -19,26 +19,25 @@ api.getAccessToken().then(() => {
 	app.post("/hook", async ({ body: { contacts: { add } } }, res) => {
 		const user = await api.getContact(add[0].id)
 		const values = user.custom_fields_values
-		let dateValue = 0
 		if(values) {
 			for (const value of values) {
 				if(value.field_name === 'День рождения') {
-					dateValue = value.values[0].value
+					const contactAge = age(value.values[0].value)
+					if(contactAge !== false) {
+						user.custom_fields_values.push({
+							field_id: 47857,
+							values: [
+								{
+									value: String(contactAge)
+								}
+							]
+						})
+				
+						await api.updateContacts({ ...user })
+					}
+					break
 				}
-			}
-			const contactAge = age(dateValue)
-			if(contactAge !== false) {
-				user.custom_fields_values.push({
-					field_id: 47857,
-					values: [
-						{
-							value: String(contactAge)
-						}
-					]
-				})
-		
-				await api.updateContacts({ ...user })
-			}
+			}	
 		}
 
 		res.send("OK");
